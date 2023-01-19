65cf7d426c22e8e954144bdbe6e37d8e4164af02
Leandro Lacayo <leandrolacayo17@gmail.com>
2023-01-19 12:03:06 -0600

    Jueves 19-01-23

---
+++ Contraseña.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jueves190123
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Adivinar la contraseña
+            int pass, cont, intentos=2023;
+            pass = 2023;
+            cont = 0;
+            while (cont <=3 ) 
+            {
+                Console.WriteLine("Digite la contraseña");
+                pass=int.Parse(Console.ReadLine());
+                if (pass == intentos)
+                {
+                    Console.WriteLine("La contraseña es correcta");
+                    break;
+                }
+                else 
+                { 
+                    Console.WriteLine("Contraseña incorrecta.");
+                    cont++;
+                }
+                if (cont==3) 
+                {
+                    Console.WriteLine("Ha superado el numero de intentos!!");
+                    
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
